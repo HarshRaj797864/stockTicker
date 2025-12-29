3605964e62364febdec9590897e11b36dbf424b3
Harsh Raj <rajharsh797864@gmail.com>
2025-12-29 11:41:49 +0530

    <fix>: Create a separate db.js to avoid creating multiple instances of connection

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -1,14 +1,4 @@
-import {PrismaClient} from "../generated/prisma/client.js";
-import {PrismaPg} from '@prisma/adapter-pg';
-import pg from 'pg';
-import 'dotenv/config';
-
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const pool = new pg.Pool({connectionString});
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({adapter});
-
+import prisma from '../db.js';
 const seedDatabase = async () => {
     try{
         console.log('starting seed');
