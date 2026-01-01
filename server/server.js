91df1badfe33cadf7cf2a50b392599011e27ea5f
Harsh Raj <rajharsh797864@gmail.com>
2026-01-01 14:01:53 +0530

    docs: Add Render.yaml for deploying monorepo

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ import app from "./app.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on ${PORT}`);
 })
